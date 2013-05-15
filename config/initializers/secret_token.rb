48b194ac81171ce79ae27832e91a9739ba5ba12e
makotoworld <makotoworld@gmail.com>
2013-05-16 07:01:06 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTest::Application.config.secret_key_base = '2fc479d89882cc4ca3d67a5e89d56c5b2e08e5fcdc2a6002e4413661d057ec78bedd2f653b6fc5432f5d3958e4072ec6a49c6eee7387ff9745d2de6cae8f15ee'
